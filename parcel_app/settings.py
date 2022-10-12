4fbdf7e2c1b5c769ac12cc63a72e7a73921e15dc
isenewo_ephraim <isenewoephr2012@gmail.com>
2022-10-12 17:34:25 +0100

    initial commit

--- parcel_app/settings.py
+++ parcel_app/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hp-x7q7dj)d@$_+pxg9*k(!n8ozx0m42(#%fwlj(9)jhi8o530'
+SECRET_KEY = env('DJANGO_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
